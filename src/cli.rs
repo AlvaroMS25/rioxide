d780512e130cb35295b2d86b1e1f2c9be326a8d5
AlvaroMS25 <alvaro2ms5@gmail.com>
2024-05-07 10:50:17 +0200

    create cli args

---
+++ src/cli.rs
@@ -0,0 +1,19 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version = "0.1")]
+#[command(author = "Alvaro")]
+#[command(about = "A simple racket interpreter")]
+#[command(long_about = "A simple racket interpreter made to learn to write interpreters")]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Option<SubCommands>
+}
+
+#[derive(Subcommand)]
+pub enum SubCommands {
+    Repl,
+    Run {
+        filename: String
+    }
+}
