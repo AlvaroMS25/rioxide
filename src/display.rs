44bf347e9fec536e4d5322378d60e0a2d09ff8d9
AlvaroMS25 <alvaro2ms5@gmail.com>
2024-05-07 11:20:47 +0200

    implement custom display for all items implementing display

--- src/display.rs
+++ src/display.rs
@@ -1,5 +1,14 @@
-use core::fmt;
+use std::fmt::{self, Display};
 
 pub trait InterpreterDisplay {
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result;
+}
+
+impl<T> InterpreterDisplay for T
+where
+    T: Display
+{
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        <Self as Display>::fmt(self, fmt)
+    }
 }
