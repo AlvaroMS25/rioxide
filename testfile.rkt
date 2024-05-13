4ae44cfd4218d9fbf0c70e202f632ee6f0b35ec8
AlvaroMS25 <alvaro2ms5@gmail.com>
2024-05-13 15:49:13 +0200

    a

--- testfile.rkt
+++ testfile.rkt
@@ -1,2 +1,5 @@
-(define (double x) (* x x))
-(define double-lambda (lambda (x) (* x x)))
+(define (double x) (list x x))
+(define double-lambda (lambda (x) (cons x x)))
+
+(double 2)
+(double-lambda 5)
